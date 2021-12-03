57b628c2315ea499b6e92668cff757fbbfeb9f67
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2021-12-04 00:21:56 +0100

    feat: Adding #152 problem solution

--- python/medium/152-Maximum-Product-Subarray.py
+++ python/medium/152-Maximum-Product-Subarray.py
@@ -1,5 +1,44 @@
 class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
+    class Solution:
+        def maxProduct(self, nums: List[int]) -> int:
+            """
+            Time complexity  O(n)
+            Space complexity O(1)
+
+            Approach: Keep track of two products: max so far, min so far.
+                      One every new calculation keep the best over all possible products
+
+            Examples:
+            [2,3,-2,4]
+        max  2,6,-2,4
+        min  2,6,-12,-12
+
+
+            maxsofar = max(maxsofar * num, minsofar*num, num)
+            minsofar = min(minsofar * num, max_sofar*num, num)
+
+
+            [0,-1,-2, 4, 3, -2, 0,4,-2]
+
+    max      0,0,  2, 8, 24, 48,0,4,0
+    min      0,-1,-2,-8,-24,-48,0,0,-8
+
+
+            [0,-48,48,-24,-6,-2,0,-8,-2]
+            """
+            best = float(-inf)
+            max_so_far = 1
+            min_so_far = 1
+
+            for num in nums:
+                new_max_so_far = max(max_so_far * num, min_so_far * num, num)
+                new_min_so_far = min(max_so_far * num, min_so_far * num, num)
+                max_so_far = new_max_so_far
+                min_so_far = new_min_so_far
+                best = max(best, min_so_far, max_so_far)
+            return best
+
+    def maxProductSecondSolution(self, nums: List[int]) -> int:
         """
         [0,-1,-2,4,3,-2,0,4,-2] => even => odds
                  ^
