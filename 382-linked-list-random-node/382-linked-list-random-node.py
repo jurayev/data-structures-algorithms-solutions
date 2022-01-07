57c0a5976c0c4e038454a5370a8626c5ba9285a0
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-07 19:42:17 +0100

    Time: 290 ms (5.01%), Space: 17.3 MB (56.84%) - LeetHub

--- 382-linked-list-random-node/382-linked-list-random-node.py
+++ 382-linked-list-random-node/382-linked-list-random-node.py
@@ -3,10 +3,37 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 from random import randint
 
 class Solution:
+     
+    def __init__(self, head: Optional[ListNode]):
+        self.head = head
 
+    def getRandom(self) -> int:
+        """
+        getRandom() -> reservoir sampling
+            Time Complexity O(N)
+            Space Complexity O(1)
+        """
+        total = 0
+        node = self.head
+        picked_element = self.head.val
+        while node:
+            if randint(0, total) == 0:
+                picked_element = node.val
+            
+            node = node.next
+            total += 1
+        return picked_element
+
+class Solution1:
+    """
+    getRandom()
+        Time Complexity O(1)
+        Space Complexity O(N)
+    """
     def __init__(self, head: Optional[ListNode]):
         self.nodes = []
         while head:
