dcc91720fc20609692f44727743e0f05b343339d
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-09 15:36:31 +0100

    Time: 1367 ms (7.14%), Space: 14.7 MB (100.00%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -1,9 +1,34 @@
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
         """
-        [   [1,1,1],
-            [1,2,3],
-            [1,2,3]]
+        XOR solution
+        
+        Complexities:
+            Time O(N^2)
+            Space O(1)
+        """
+        n = len(matrix)
+        
+        xored = 0
+        for i in range(1, n+1):
+            xored ^= i
+        
+        for i in range(n):
+            row = xored
+            col = xored
+            for j in range(n):
+                row ^= matrix[i][j]
+                col ^= matrix[j][i]
+                
+            if max(row, col) != 0:
+                return False
+        return True
+        
+    def checkValidWithSets(self, matrix: List[List[int]]) -> bool:
+        """
+        [[1,1,1],
+         [1,2,3],
+         [1,2,3]]
         
         Complexities:
             Time O(N^2)
