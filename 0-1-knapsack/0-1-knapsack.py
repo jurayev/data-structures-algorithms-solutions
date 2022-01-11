049b15841f4893cdc63792679bffedfe900bc2be
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-11 23:10:51 +0100

    feat: Solution for "0/1 knapsack" problem

---
+++ 0-1-knapsack/0-1-knapsack.py
@@ -0,0 +1,22 @@
+def solve_knapsack_brute_force(profits, weights, capacity):
+    """
+      profit = 0 # 4 # 3 #
+      weight = 0 # 2 #
+      max = 9
+      Items: { Apple, Orange, Banana, Melon }
+      Weights: { 2, 3, 1, 4 }
+                          ^
+      Profits: { 4, 5, 3, 7 }
+    """
+    max_profit = [0]
+    solve_brute_force(profits, weights, capacity, max_profit, 0, 0, 0)
+    return max_profit[0]
+
+
+def solve_brute_force(profits, weights, capacity, max_profit, weight, profit, start):
+    if weight > capacity:
+        return
+    for i in range(start, len(profits)):
+        solve_brute_force(profits, weights, capacity, max_profit, weight + weights[i], profit + profits[i], i + 1)
+
+    max_profit[0] = max(max_profit[0], profit)
