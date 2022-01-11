910bf5b628a4abcaecbb92bc454a33047951860a
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-11 23:11:51 +0100

    feat: Solution for "0/1 knapsack" problem

--- 0-1-knapsack/0-1-knapsack.py
+++ 0-1-knapsack/0-1-knapsack.py
@@ -7,6 +7,9 @@ def solve_knapsack_brute_force(profits, weights, capacity):
       Weights: { 2, 3, 1, 4 }
                           ^
       Profits: { 4, 5, 3, 7 }
+
+    Time Complexity: O(2^N)
+    Space Complexity O(2^N)
     """
     max_profit = [0]
     solve_brute_force(profits, weights, capacity, max_profit, 0, 0, 0)
