9c6c916053b1957fc2aea926ece60f19f5ce8889
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-12 09:51:54 +0100

    Time: 154 ms (29.71%), Space: 16.8 MB (26.36%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.py
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.py
@@ -6,13 +6,29 @@
 #         self.right = right
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        """
+        Searching insert position using BST properties.
+        Complexities:
+            Time O(log N) - N is the number of nodes
+            Space O(log N) - for recursion stack
+        """
+        if not root:
+            return TreeNode(val)
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
+
+        
+    def insertIntoBST1(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         if not root:
             return TreeNode(val)
         self.insert(root, val)
         return root
     
     
-    def insert(self, root, val):
+    def insert1(self, root, val):
         if val < root.val:
             if not root.left:
                 root.left = TreeNode(val)
