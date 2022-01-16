364eeae387aab1a4d8429fc37d9132d0d7608252
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-16 15:41:25 +0100

    Time: 28 ms (100.00%), Space: 14 MB (100.00%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        """
+        Q2 from Weekly contest 276
+        Submission time: 0:25:03
+        Time spent: 10 mins + 5 mins penalty
+         
+        Time Complexity: O(D), where D is the number of doubles
+        """
+        moves = 0
+        while maxDoubles and target > 1:
+            if target % 2 == 0:
+                target //= 2
+                maxDoubles -= 1
+            else:
+                target -= 1
+            moves += 1
+        return moves + target - 1
