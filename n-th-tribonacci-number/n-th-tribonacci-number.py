7a7a312b5c317962740dc8456cdcf5d7e6a124f7
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-17 23:43:01 +0100

    Time: 39 ms, Memory: 14.4 MB - LeetHub

--- n-th-tribonacci-number/n-th-tribonacci-number.py
+++ n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,14 +1,14 @@
 class Solution:
     cache = {0:0, 1:1, 2:1}
     
-    def tribonacci1(self, n: int) -> int:
+    def tribonacci(self, n: int) -> int:
         if n in self.cache:
             return self.cache[n]
         
         self.cache[n] = self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
         return self.cache[n]
 
-    def tribonacci(self, n: int) -> int:
+    def tribonacci1(self, n: int) -> int:
         
         for i in range(3, n+1):
             self.cache[i] = self.cache[i-1] + self.cache[i-2] + self.cache[i-3]
