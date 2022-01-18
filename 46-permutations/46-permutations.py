e89058093cf9c8ecf901bc755ad9f558af70aca0
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-18 19:38:38 +0100

    Time: 76 ms (7.59%), Space: 14.6 MB (14.73%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,6 +1,27 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         result = []
+        if not nums:
+            return result
+        
+        q = deque([[nums[0]]])
+        while q:
+            curr_arr = q.popleft()
+            n = len(curr_arr)
+            if n == len(nums):
+                result.append(curr_arr)
+                continue
+
+            for i in range(n+1):
+                next_arr = curr_arr[:]
+                next_arr.insert(i, nums[n])
+                q.append(next_arr)
+
+        return result
+        
+        
+    def permuteSwaps(self, nums: List[int]) -> List[List[int]]:   
+        result = []
         self.find(nums.copy(), 0, result)
         return result
 
