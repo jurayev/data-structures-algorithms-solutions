a2f2bd1a2700b1ba2234b603649d6e166f284596
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-19 23:17:51 +0100

    Time: 40 ms (51.10%), Space: 14.5 MB (68.25%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,51 @@
+from collections import deque
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        self.dfs(result, [], n, 0, 0)
+        return result
+        
+    def dfs(self, result, brackets, n, opening, closing):
+        if opening < closing: # base case 1 # unbalanced permutation
+            return
+        if opening > n or closing > n: # base case 2 # unbalanced permutation
+            return
+        if len(brackets) == n*2: # base case 3 # making sure the balance
+            result.append("".join(brackets))
+            return
+        brackets.append("(")
+        self.dfs(result, brackets, n, opening+1, closing)
+        brackets.pop()
+        
+        brackets.append(")")
+        self.dfs(result, brackets, n, opening, closing+1)
+        brackets.pop()
+        
+    def generateParenthesisTopDown(self, n: int) -> List[str]:
+        """
+        n = 2
+
+        ()(), (())
+        ^
+
+        Approach: Using BFS start with single brackets, trying all possible
+                  permutations. Keep track of opening and closing variables
+                  for balancing purposes.
+        Time Complexity: O(N!) Generating all possible permutations of N brackets times 2
+        Space Complexity O(N!)
+        """
+        result = []
+        
+        q = deque([(["("], 1, 0)])
+        while q:
+            brackets, opening, closing = q.popleft()
+            if opening > n or closing > n: # unbalanced permutation
+                continue
+            if len(brackets) == n*2: # making sure the balance
+                result.append("".join(brackets))
+                continue
+            q.append((brackets + ["("], opening+1, closing))
+            if opening > closing: # making sure the balance
+                q.append((brackets + [")"], opening, closing+1))
+        return result
