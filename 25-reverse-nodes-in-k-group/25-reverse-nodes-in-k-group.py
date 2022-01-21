c97e64ef29a5f4f4457705b687c642e3079620d8
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-21 21:52:17 +0100

    Time: 96 ms (8.78%), Space: 15.2 MB (44.81%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
@@ -5,6 +5,54 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        """
+        Approach: reversing the nodes in k groups in-place.
+        Time O(N)
+        Space O(1)
+        """
+        
+        dummy = ListNode()
+        new_head = dummy
+        
+        node = head
+        while node:
+            rev_head = self.reverse(node, k)
+            new_head.next = rev_head
+            new_head = node
+            node = node.next
+        return dummy.next
+    
+    def reverse(self, head, k):
+        """
+        k = 2
+        1->2->3->4->5
+              ^
+        2->1->4->3->5
+                    ^
+        """
+        node = head
+        count = 0
+        while count < k and node:
+            count += 1
+            node = node.next
+        if count < k: return head
+        
+        node, prev = head, None
+        while k and node:
+            k -= 1
+            next_node = node.next
+            node.next = prev
+            prev = node
+            node = next_node
+        head.next = node
+        return prev
+    
+    def reverseKGroup1(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        """
+        Simple approach using extra space and reversing the nodes in k groups
+        Time O(N)
+        Space O(N)
+        """
         if k < 2:
             return head
         nodes = []
