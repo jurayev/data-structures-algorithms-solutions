b7481e3cc1ad526a4271c4de94299ee7f9ffa8bc
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-23 18:37:22 +0100

    Time: 2154 ms (20.00%), Space: 42.7 MB (40.00%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        """
+        [3,1,-2,-5,2,-4]
+         ^
+        """
+        plus = [num for num in nums if num > 0]
+        minus = [num for num in nums if num < 0]
+        
+        p_idx, m_idx = 0, 0
+        
+        while p_idx < len(plus):
+            nums[p_idx+m_idx] = plus[p_idx]
+            p_idx += 1
+            nums[p_idx+m_idx] = minus[m_idx]
+            m_idx += 1
+        return nums
+        
