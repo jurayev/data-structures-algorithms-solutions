4756a98d2965c79590e68ba2f766edfb6b0253f0
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-30 12:43:50 +0100

    Time: 228 ms (70.62%), Space: 25.5 MB (83.32%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,6 +3,28 @@ class Solution:
         """
         [1,2,3,4,5,6,7]
         
+        [7,6,5,4,3,2,1]
+        
+        [5,6,7,1,2,3,4]
+                   j
+        """
+        n = len(nums)
+        k %= n
+        self.reverse(nums, 0, n-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, n-1)
+        
+    def reverse(self, nums, i, j):
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+                
+        
+    def rotate1(self, nums: List[int], k: int) -> None:
+        """
+        [1,2,3,4,5,6,7]
+        
         """
         k %= len(nums)
         j = 0
