ae3bef17827c67cd9d422434e8613a6d37465bc5
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-31 20:15:57 +0100

    Time: 304 ms (92.54%), Space: 21.9 MB (47.50%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return sorted(nums)
+    
+    def buble_sort(self, nums: List[int]) -> List[int]:
+        """
+        Buble Sort Impelementation
+        
+        Time O(N^2)
+        Space O(1)
+        """
+        size = len(nums)
+        for i in range(size):
+            for j in range(size-1):
+                if nums[j] >= nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+                
+        return nums
+                
+        
+        
