6de81d022f5c93dde08bcc358ecb73e18743af65
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-01-31 19:45:03 +0100

    Time: 93 ms (17.39%), Space: 13.9 MB (99.25%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -1,13 +1,5 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        """
-        i - customer
-        j - bank
-
-        [[1,5],
-        [7,3],
-        [3,5]]
-        """
         best = 0
         for account in accounts:
             best = max(best, sum(account))
