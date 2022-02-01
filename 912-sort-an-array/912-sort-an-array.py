b740117aebadfeeab5dbbf9fd365b801d8183927
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-01 20:43:39 +0100

    Time: 324 ms (91.80%), Space: 21.4 MB (75.67%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -1,14 +1,44 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        return sorted(nums)
+        #return self.merge_sort(nums)
+        #return self.quick_sort(nums)
+        return self.heap_sort(nums)
+        #return self.insertion_sort(nums)
+        #return self.bubble_sort(nums)
+        #return self.selection_sort(nums)
+        #return sorted(nums)
+    
+    def heap_sort(self, nums):
+        """
+        Heap Sort Impelementation
+        
+        Time O(N log N)
+        Space O(1)
+        """
+        from heapq import heapify, heappop
+        min_heap = nums[:]
+        heapify(min_heap)
+        
+        index = 0
+        while min_heap:
+            min_element = heappop(min_heap)
+            nums[index] = min_element
+            index += 1
+        return nums
     
     def insertion_sort(self, nums: List[int]) -> List[int]:
         """
-        Buble Sort Impelementation
+        Insertion Sort Impelementation
         
         Time O(N^2)
         Space O(1)
         """
+        n = len(nums)
+        for i in range(0, n):
+            for j in range(i, 0, -1):
+                if nums[j-1] <= nums[j]:
+                    break
+                nums[j-1], nums[j] = nums[j], nums[j-1] 
         return nums
         
     def selection_sort(self, nums: List[int]) -> List[int]:
@@ -18,9 +48,16 @@ class Solution:
         Time O(N^2)
         Space O(1)
         """
+        n = len(nums)
+        for i in range(0, n):
+            min_el_idx = i
+            for j in range(i+1, n):
+                min_el_idx = j if nums[j] < nums[min_el_idx] else min_el_idx
+            nums[i], nums[min_el_idx] = nums[min_el_idx], nums[i]			
         return nums
+
     
-    def buble_sort(self, nums: List[int]) -> List[int]:
+    def bubble_sort(self, nums: List[int]) -> List[int]:
         """
         Buble Sort Impelementation
         
