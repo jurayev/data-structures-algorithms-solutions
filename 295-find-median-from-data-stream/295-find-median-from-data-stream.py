de794b624530ae137f93e4d9b83fd02e488ef705
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-01 23:41:35 +0100

    Time: 832 ms (43.40%), Space: 36.3 MB (27.78%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -1,17 +1,40 @@
 from heapq import heappush, heappop, heappushpop
-      
+from sortedcontainers import SortedList
+
 class MedianFinder:
-    """
-    ["MedianFinder","addNum","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian"]
+    """ 
+    Time O(N log N)
+    Space O(N)
+    
+    Test-case:
+            ["MedianFinder","addNum","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian"]
     [[],[1],[2],[],[3],[],[-3],[],[-3],[],[-3],[],[0],[],[-1],[],[-10],[],[4],[],[-1],[]]
-                            ^
+    """ 
+    def __init__(self):
+        self.elements = SortedList() # B-tree, most operations O(log N)
+                                
+
+    def addNum(self, num):  # O(log N)
+        self.elements.add(num)
+            
+    def findMedian(self): # O(1) because mid element is always a root element of B-tree
+        mid_idx = len(self.elements) // 2
+        if len(self.elements) % 2 == 0:
+            return (self.elements[mid_idx-1] + self.elements[mid_idx]) / 2
+
+        return self.elements[mid_idx]
+    
+class MedianFinderTwoHeaps:
+    """
+    Time O(N log N)
+    Space O(N)
     """ 
     def __init__(self):
         self.max_elements = [] # min heap # [2,3]  -> 
         self.min_elements = [] # max heap # [-1,-2] ->
                                 
 
-    def addNum(self, num):  # 1 # 2 # 3
+    def addNum(self, num):  # O(log N)
         if len(self.max_elements) == len(self.min_elements):
             heappush(self.min_elements, -num)
             max_value = -heappop(self.min_elements)
@@ -21,7 +44,7 @@ class MedianFinder:
             min_value = -heappop(self.max_elements)
             heappush(self.min_elements, min_value)
             
-    def findMedian(self):
+    def findMedian(self): # O(1)
         if len(self.max_elements) == len(self.min_elements):
             return (self.max_elements[0] + (-self.min_elements[0])) / 2
         return self.max_elements[0]
