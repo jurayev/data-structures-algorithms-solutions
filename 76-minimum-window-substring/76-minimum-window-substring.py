e0d9a542bee55279ae46292c747030cf8e6014ab
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-05 10:40:19 +0100

    Time: 128 ms (63.64%), Space: 14.7 MB (97.15%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        """
+        s = "ADOBECODEBANC", t = "ABC"   t count = 3
+                       ^                   
+                         ^
+                                  010
+        s count = 0
+        
+        """
+        
+        counts = collections.Counter(t) 
+        counter = len(t)
+        min_size = float("inf")
+        min_start, min_end = 0, -1
+        start, end = 0, 0
+        
+        while end < len(s):
+            end_char = s[end]
+            if end_char in counts:
+                counts[end_char] -= 1
+                if counts[end_char] >= 0:
+                    counter -= 1
+                
+            while counter == 0:
+                if end - start + 1 < min_size:
+                    min_start, min_end = start, end
+                    min_size = end - start + 1
+                start_char = s[start]
+                if start_char in counts:
+                    counts[start_char] += 1
+                    if counts[start_char] > 0:
+                        counter += 1
+                    
+                start += 1
+                
+            end += 1
+        return s[min_start:min_end+1]
