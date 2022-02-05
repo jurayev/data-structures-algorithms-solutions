8f6690e010ec22d58d02e1700471e7e4717c6f1c
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-05 11:57:06 +0100

    Time: 231 ms (24.78%), Space: 18.9 MB (9.45%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.py
@@ -0,0 +1,44 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        """
+        [1,2,8,15]
+                ^
+        
+        [3,6,10,18]
+              ^
+         
+         [1,6] [8,10] []
+        
+        1. Solve by merging overlapping intervals
+        2. Solve using scan line: 
+            put all start and end separately
+            sort both events
+        """
+        
+        start_events = sorted([event[0] for event in intervals])
+        end_events = sorted([event[1] for event in intervals])
+        
+        merged_events = []
+        
+        start_idx, end_idx = 0, 0
+        
+        while start_idx < len(start_events):
+            start = start_events[start_idx]
+            end = end_events[end_idx]
+            if not merged_events:
+                merged_events.append([start, end])
+            elif start <= end:
+                end_idx += 1
+                start = start_events[start_idx]
+                end = end_events[end_idx]
+                merged_events[-1][1] = end_events[end_idx]
+            else:
+                end_idx += 1
+                start = start_events[start_idx]
+                end = end_events[end_idx]
+                merged_events.append([start, end])
+            start_idx += 1
+            
+        return merged_events
+        
+        
