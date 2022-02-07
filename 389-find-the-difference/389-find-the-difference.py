acd76e9a481da5416a3fbd2a8eaebe8e711a5e63
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-07 21:57:46 +0100

    Time: 34 ms (78.02%), Space: 14 MB (78.93%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,5 +1,20 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        """
+        Time O(M+N)
+        Space O(1)
+        """
+        single_number = 0
+        for letter in s + t:
+            single_number ^= ord(letter)
+        return chr(single_number)
+        
+        
+    def findTheDifferenceCounter(self, s: str, t: str) -> str:
+        """
+        Time O(N+M)
+        Space O(N+M)
+        """
         s_counts = collections.Counter(s)
         t_counts = collections.Counter(t)
         
