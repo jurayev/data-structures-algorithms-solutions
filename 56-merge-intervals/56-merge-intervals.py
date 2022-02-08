9ddc8e8d10953448c944d510e635bb204d2d0e5a
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-08 15:02:18 +0100

    Time: 436 ms (5.09%), Space: 21 MB (9.83%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,5 +1,65 @@
+class Event:
+    def __init__(self, time, event_type):
+        self.time = time
+        self.type = event_type
+        
+    def __lt__(self, other):
+        if self.time == other.time:
+            return self.type < other.type
+        return self.time < other.time
+        
+class Type:
+    START = 0
+    END = 1
+
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        """
+        Sweep Line Algo
+        Time O(N log N)
+        Space O(N)
+        
+        Test-cases:
+            [[1,3],[2,6],[8,10],[15,18]]
+            [[1,4],[4,5]]
+            [[1,3],[2,6],[6,10],[9,18]]
+            [[1,4],[0,0]]
+            [[2,3],[4,5],[6,7],[8,9],[1,10]]
+            [[1,1]]
+            
+        Dry run:
+            [1,3], [2,6] [8,10] [15, 18]
+            balance = 0
+            start = 0
+
+            [(1,s), (2,s), (3,e), (6, e), (8, s), (10,e), (15,s), (18, e)]
+              ^
+        """
+        
+        events = []
+        for interval in intervals:
+            events.append(Event(interval[0], Type.START))
+            events.append(Event(interval[1], Type.END))
+        
+        events.sort()
+        merged_events = []
+        start = 0
+        balance = 0
+
+        for event in events:
+            if balance == 0:
+                start = event.time
+            if event.type == Type.START:
+                balance += 1
+            else:
+                balance -= 1
+            if balance == 0:
+                merged_events.append([start, event.time])
+            
+            
+        return merged_events
+    
+    def merge1(self, intervals: List[List[int]]) -> List[List[int]]:
         """
         [1,2,8,15]
                 ^
