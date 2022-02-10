bea4e37e4d487683f74a126afd39eff4212eba03
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-10 22:01:49 +0100

    Time: 537 ms (61.41%), Space: 16.2 MB (71.26%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.py
@@ -0,0 +1,51 @@
+from heapq import heappush, heappop
+from collections import defaultdict
+class Edge:
+	def __init__(self, source, dest, cost):
+		self.source = source
+		self.dest = dest
+		self.cost = cost
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], nodes: int, start_node: int) -> int:
+        graph = defaultdict(list)
+        for source, dest, cost in times:
+            graph[source].append(Edge(source, dest, cost))
+
+        # Implement Dijkstra, returns all costs
+        costs = self.dijkstra(graph, start_node, nodes)
+        # vertex : cost mapping
+        # for every cost select the max cost and return
+        max_cost = max(costs)
+        return max_cost if max_cost != float("inf") else -1
+    
+    def dijkstra(self, graph, start_node, nodes):
+        """
+        start = 2
+        in_progess = [(0,2)* (1,1)* (1,3)* (2,4)*]
+                  1  2    3  4    5
+        costs = [1,  0,  1, 2,  inf]
+        visite = {2, 1, 3}
+        """
+        costs = [float("inf") for _ in range(nodes)]
+        costs[start_node-1] = 0
+        visited = set()
+        in_progress = []
+        heappush(in_progress, (0, start_node))
+        while in_progress:
+            cost, node = heappop(in_progress)
+            if costs[node-1] < cost:
+                # better cost for this node already found
+                continue
+            visited.add(node)
+            for edge in graph[node]:
+                if edge.dest in visited:
+                    # already visited
+                    continue
+                new_cost = cost + edge.cost
+                if new_cost < costs[edge.dest-1]:
+                    # relax the edge
+                    costs[edge.dest-1] = new_cost
+                    heappush(in_progress, (new_cost, edge.dest))
+        return costs
+        
