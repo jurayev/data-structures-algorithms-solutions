d6c9d036b6c279de4db81fc3b6d6e20d9c1fcc22
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-11 23:23:32 +0100

    Time: 131 ms (77.45%), Space: 16.5 MB (6.55%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -1,8 +1,66 @@
+class Edge:
+    
+    def __init__(self, dest, cost):
+        self.cost = cost
+        self.dest = dest
+        
+class Node:
+    
+    def __init__(self, cost, stops, node_id):
+        self.cost = cost
+        self.stops = stops
+        self.id = node_id
+        
+    def __lt__(self, other):
+        if self.cost == other.cost:
+            return self.stops < other.cost
+        return self.cost < other.cost
+    
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        return self.bellman_ford_min_cost(flights, n, src, dst, k)
+        # TODO impelement as dijkstra
+        return self.dijkstra(flights, n, src, dst, k)
+        #return self.bellman_ford_min_cost(flights, n, src, dst, k)
         
+    def dijkstra(self, flights, cities, src, dst, stops):
+        """
+        [(0, 0), (100, 1), (200, 2), (500, 2)]
         
+        [(0, 0, 0), (1, 1, 1), (5, 2, 1), (2, 2, 2), (6, 3, 2), (4, 3, 3)]
+        4
+        [[0,1,1],[0,2,5],[1,2,1],[2,3,1]]
+        0
+        3
+        1
+        """
+        graph = collections.defaultdict(list)
+        for source, dest, cost in flights:
+            graph[source].append(Edge(dest, cost))
+        
+        costs = [float("inf") for _ in range(cities)]
+        curr_stops = [float("inf") for _ in range(cities)]
+        costs[src] = 0
+        curr_stops[src] = 0
+        
+        cheapest = []
+        heappush(cheapest, Node(0, 0, src))
+        while cheapest:
+            node = heappop(cheapest)
+            if node.id == dst:
+                return node.cost
+            if node.stops > curr_stops[node.id]:
+                continue
+            if node.stops > stops: 
+                continue
+            curr_stops[node.id] = node.stops 
+            for edge in graph[node.id]:
+                new_cost = node.cost + edge.cost
+                if new_cost < costs[edge.dest]:
+                    costs[edge.dest] = new_cost  
+                heappush(cheapest, Node(new_cost, node.stops + 1, edge.dest))
+    
+        #print(costs)
+        return costs[dst] if costs[dst] < float("inf") else -1
     def bellman_ford_min_cost(self, flights, airports, src, dst, at_most_stops):
         """
         
@@ -24,6 +82,11 @@ class Solution:
               50	 \		/ 25
                         3
                         
+5
+[[0,1,5],[1,2,5],[0,3,2],[3,1,2],[1,4,1],[4,2,1]]
+0
+2
+2
 4
 [[0,1,1],[0,2,5],[1,2,1],[2,3,1]]
 0
