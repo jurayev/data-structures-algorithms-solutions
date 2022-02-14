0a17ce20250c3157708d1a87529aee0f25c5f193
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-14 23:19:20 +0100

    Time: 240 ms (99.59%), Space: 15.3 MB (51.84%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -16,10 +16,6 @@ class Solution:
         5
         [[0,1,2],[0,4,8],[1,2,3],[1,4,2],[2,3,1],[3,4,1]]
         10
-        
-        
-        Time O(V + E log V)
-        Space O(E)
         """
         graph = collections.defaultdict(list)
         for source, dest, cost in edges:
@@ -28,6 +24,7 @@ class Solution:
 
         cities = []
         for node in range(n):
+            #costs = self.bellman_ford(node, edges, distanceThreshold, n)
             costs = self.dijkstra(node, graph, distanceThreshold, n)
             total_cities = 0
             for cost in costs:
@@ -37,8 +34,29 @@ class Solution:
         smallest_connections = [i for i in range(n) if cities[i] == min_cities]
         return smallest_connections[-1] if smallest_connections else -1
 
-
+    def bellman_ford(self, start_node, edges, max_distance, total_nodes):
+        """
+        Time O(V*E)
+        Space O(E)
+        
+        TLE
+        """
+        costs = [float(inf) for _ in range(total_nodes)]
+        costs[start_node] = 0
+        for node in range(total_nodes):
+            for source, dest, cost in edges:
+                new_cost = cost + costs[source]
+                costs[dest] = min(costs[dest], new_cost)
+                new_cost = cost + costs[dest]
+                costs[source] = min(costs[source], new_cost)
+                
+        return [cost for cost in costs if cost <= max_distance]
+    
     def dijkstra(self, start_node, graph, max_distance, total_nodes): 
+        """
+        Time O(V + E log V)
+        Space O(E)
+        """
         costs = [float(inf) for _ in range(total_nodes)]
         costs[start_node] = 0
         min_heap = []
