0438540e86ef472eb4c9b8e9da46b696f7b569b6
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-20 18:06:03 +0100

    Time: 112 ms (51.63%), Space: 17.3 MB (12.43%) - LeetHub

--- 214-shortest-palindrome/214-shortest-palindrome.py
+++ 214-shortest-palindrome/214-shortest-palindrome.py
@@ -2,35 +2,43 @@ class Solution:
     def shortestPalindrome(self, s: str) -> str:
         """
         
-        aacecaaa   -> aaacecaaa
-        aaacecaa
+        "aaacecaa"
+        "aacecaaa"
         
-        aaacecaa   -> aacecaaacecaa
-           j 
-        aacecaaa
-                ^
-        aacecaaa$aaacecaa
-        
-        abcd       -> dcbabcd
-        dcba
-        ^
-       
-        accd       -> dccaccd
-        dcca
-        
-        baa        -> aabaa
-        aab
-        aa         -> 
-       ^
-        bcaabbb    -> aacbcaabbb
-        bbbaacb                 ^
-              ^
+        aaacecaa # aacecaaa
         Sol 1. 
             1. Take all substring
             2. Try to find the longest leftmost palindrome in it (start, end indexes)
             3. if start index < 0 -> just prepend everything after end index
             4. if start index >= 0 -> prepend everything after start index
+            
+        Sol 2. KMP
+            1. Make pattern of the original string
+            2. compare reverse string with original string (original string is considered as a substring here)
+            
+        Sol 2. Optimized KMP
+            1. Make pattern of the original string + reverse string
+            2. the last position in patter would indicate the longest prefix suffix
+            3. take last position as len of prefix-suffix and take a substring from reversed till this position.
         """
+        rev_s = s[::-1]
+        search_string = s + "#" + rev_s
+        n = len(search_string)
+        pattern = [0] * n
+        i, j = 0, 1
+        while j < n:
+            if search_string[i] == search_string[j]:
+                pattern[j] = i + 1
+                i += 1
+                j += 1
+            elif i > 0:
+                i = pattern[i-1]
+            else:
+                j += 1
+        end_index = len(s) - pattern[-1]
+        return rev_s[:end_index] + s
+
+    def shortestPalindrome2(self, s: str) -> str:
         n, m = len(s), len(s)
         rev_str = s[::-1]
         # buidling pattern for original string
