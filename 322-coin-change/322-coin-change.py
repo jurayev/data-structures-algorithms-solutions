9fedd3e79d7debc83d27227b515f0130c0f8a48b
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-22 00:20:26 +0100

    Time: 1968 ms (48.80%), Space: 19.9 MB (18.50%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,11 +1,32 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        cache = {}
+        min_count = self.min_coins(amount, coins, cache)
+        return min_count if min_count < float(inf) else -1
+    
+    def min_coins(self, rem_amount, coins, cache):
+        if rem_amount < 0:
+            return float("inf")
+        if rem_amount == 0:
+            return 0
+        
+        if rem_amount in cache:
+            return cache[rem_amount]
+        
+        min_count = float("inf")
+        for coin in coins:
+            min_count = min(min_count, self.min_coins(rem_amount - coin, coins, cache) + 1)
+        cache[rem_amount] = min_count
+        return min_count
+        
+    
+    def coinChange1(self, coins: List[int], amount: int) -> int:
         """
-        [1,2,5] amount = 11
+        [1,2,5] amount = 7
          j
-        [0,1,2,3,4,5,6,7,8,9,10,11]
-        [0,1,1,2,2,1,6,7,8,9,10,11]
-           i
+        [0,1,2,3,4,5,6,7]
+        [i,1,1,2,2,3,i,i]
+                   i
  
         State [j]
         Value function -> min_coins[i] min coins to get this amount
