054159f833dc62f509218ea31e8d2c0470b13047
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-26 21:21:33 +0100

    Time: 69 ms (60.71%), Space: 14.2 MB (60.86%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -0,0 +1,45 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        """
+        
+        abc
+        
+        a.c
+        
+        .*
+        
+        abcdddmmmm
+        ^
+        
+        .*abcd*dm..
+        ^
+        
+        recurrence:
+        x. match -> move i, j
+        x. "." move forwards i, j
+        -. "d*" move forwards i as long as d is match, then move j once
+        x. "d*" move forwards j, backtrack move i if char1 == char2
+        x. ".*" move forwards i, backtrack move j
+        x. "*" -> i, j+1
+        
+        base cases:
+            1. i > n: false
+            2. j > m: false
+            3. i == n and j == m: true
+            
+        answer: match(0, 0)
+        """
+        @lru_cache(None)
+        def match(i, j):
+            if j >= len(p):
+                return i == len(s)
+            wildcard_match = j+1 < len(p) and p[j+1] == "*"
+            match_curr_char = i < len(s) and p[j] in [".", s[i]]
+            #   abc        abc     abc  abc
+            #   .*abc      a.*c    aa*  ab.*.*
+            if wildcard_match:
+                return match(i, j+2) or match_curr_char and match(i+1, j)
+
+            return match_curr_char and match(i+1, j+1)
+        
+        return match(0, 0)
