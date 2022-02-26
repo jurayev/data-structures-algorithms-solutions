a5f419ffd024c72a8844486a8b119a96e0ecb37f
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-26 11:22:30 +0100

    Time: 310 ms (19.13%), Space: 18 MB (19.47%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -8,7 +8,46 @@ class Solution:
         "horse" "ros"
            i
                    j
+                   
+        0x
+        xx
+        abcd
+      ""23210
+      ""12210
+       a01210   2x4  
+       b10110
+        00000
+        
+          b c
+        0 1 2
+       a1 1 2
+       b2 1 2
+        
+        Time O(N*N), where N is the max of (len1, len2)
+        Space O(N*N), where N is the max of (len1, len2)
         """ 
+        n, m = len(word1), len(word2)
+        dp = [[0 for i in range(m+1)] for i in range(n+1)]
+        
+        for i in range(n+1):
+            dp[i][0] = i
+        for j in range(m+1):
+            dp[0][j] = j
+        
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1
+
+        return dp[n][m]
+    
+    def minDistanceRec(self, word1, word2):
+        """
+        Time O(N*M)
+        Space O(N*M)
+        """
         n, m = len(word1), len(word2)
         
         @lru_cache(None)
