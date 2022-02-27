dd5e049804e62608327e0c3a7a7ec776e6148ae0
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-02-28 00:00:33 +0100

    Time: 6532 ms (60.29%), Space: 16.9 MB (72.40%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.py
+++ 312-burst-balloons/312-burst-balloons.py
@@ -16,11 +16,25 @@ class Solution:
         2. recurrence:
                     1. burst(j, left, right) = max(burst([j+1, j+2, j+3, left, right))
                     2. 
-                    burst(l, j-1) + burst(j+1, r) + nums[l] * nums[j] * nums[r]
+                    burst(l, j) + burst(j, r) + nums[l] * nums[j] * nums[r]
         base cases:
             
         answer -> burst(0, n-1)
+        
         """
+        nums = [1] + nums + [1]
+        n = len(nums)
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        
+        for left in range(n-2, 0,-1):     
+            for right in range(left, n-1):
+                for j in range(left, right+1):
+                    points = dp[left][j-1] + dp[j+1][right] + nums[left-1] * nums[right+1] * nums[j]
+                    dp[left][right] = max(dp[left][right], points)
+        # for row in dp:
+        #     print(dp)
+        return dp[1][n-2]
+        
         @lru_cache(None)
         def burst(left, right):
             if left >= right:
@@ -33,4 +47,4 @@ class Solution:
                 points = burst(left, j) + burst(j, right) + left_num * right_num * nums[j]
                 max_points = max(max_points, points)
             return max_points
-        return burst(-1, len(nums))
+        #return burst(-1, len(nums))
