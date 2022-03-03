33f92b622829ca58e0ffb003e0360879c42b1284
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-03-03 23:22:41 +0100

    Time: 87 ms (5.26%), Space: 73.2 MB (5.46%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,9 +1,20 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+        if not t:
+            return False
+        
+        if s[0] == t[0]:
+            return self.isSubsequence(s[1:], t[1:])
+        
+        return self.isSubsequence(s, t[1:])
+    
+    def isSubsequence1(self, s: str, t: str) -> bool:
         """
-        s = "abcr", t = "ahcbgddcr"
+        s = "abck", t = "ahcbgddcr"
                 ^
-                                 ^
+                                  ^
         "aaaaaa"
              ^
         "bbaaaa"
