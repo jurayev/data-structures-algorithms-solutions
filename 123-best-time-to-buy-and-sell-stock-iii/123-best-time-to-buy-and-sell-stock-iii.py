07259ff2c48370f4649e02939a8cf57c6c52b11a
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-03-07 21:09:45 +0100

    Time: 4588 ms (7.90%), Space: 179 MB (7.58%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,58 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+         0 1 2 3 4 5 6 7
+        [3,3,5,0,0,3,1,4] k = 2 ans = 6
+           i j
+                 i     j
+                 i j
+                     i
+                       j
+           buy  sell
+        1. 3     5
+        2. 3     5
+        
+            0  1   2 3 4 5 6 7
+         b  3  3   3 0 0 3 1 4
+         s  0  0   2 0 0 3 3 3
+         
+         4 3 2 1 0
+         b s b s
+         compute suffixes
+         for i in range(n)
+         dp(i, k) = max(dp(i, k), dp(i+1, k), dp(i+1, k-1))
+         
+         base cases:
+            if i >= len:
+                return 0
+            if k == 0:
+                return 0
+        """
+        cache = defaultdict(dict)
+        @lru_cache
+        def get_max_profit(i, k):
+            if i >= len(prices):
+                return 0
+            # if i in cache:
+            #     return cache[i]
+            if k == 0:
+                return 0
+            
+            # buy or sell, 4 transactions
+            # hold
+            
+            can_buy = k % 2 == 0
+            # hold
+            profit = get_max_profit(i+1, k)
+            if can_buy:
+                # buy
+                profit = max(profit, -prices[i] + get_max_profit(i+1, k-1))
+            else:
+                # sell
+                profit = max(profit, prices[i] + get_max_profit(i+1, k-1))
+            # cache[i] = max_profit
+            return profit
+        
+        return get_max_profit(0, 4)
+            
+            
