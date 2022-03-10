6933bd69a2de4aa386280d17e6543023eab9928f
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-03-10 23:30:10 +0100

    Time: 1008 ms (45.79%), Space: 31.6 MB (55.24%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -7,12 +7,25 @@ class Solution:
         2. Use mono decreasing queue
             Time O(N)
             Space O(N)
+        3. Scan from right to left and keep a tallest building
         
         [4,2,3,3,1]
-                 ^
+         ^
+        3
         [0, 3, 4]
         """
-        
+        tallest = float("-inf")
+        buildings = []
+        total = len(heights)
+        for index in reversed(range(total)):
+            building = heights[index]
+            if building > tallest:
+                tallest = building
+                buildings.append(index)
+                
+        return buildings[::-1]
+    
+    def mono_queue(heights):
         mono_stack = []
         for index in range(len(heights)):
             height = heights[index]
