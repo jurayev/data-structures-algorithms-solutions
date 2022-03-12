186a4ef58cd8486c3416871ab405992d06be82c6
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-03-12 14:28:55 +0100

    Time: 40 ms (84.03%), Space: 14.1 MB (34.52%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.py
+++ 249-group-shifted-strings/249-group-shifted-strings.py
@@ -4,7 +4,20 @@ class Solution:
         ["abc","bcd","acef","xyz","az","ba","a","z"]
         
         
-        az -> bx
+        az -> az
+        ba -> aa
+        cb -> 
+        
+        d = 25
+        a -> 0 + 25 % 26 -> 25 -> z
+        z -> 122 + 25 % 26 -> 17 -> 
+             147
+        d = 25
+        b -> 1 + 25 % 26 -> 0 -> a
+        a -> 0 + 25 % 26 -> 25 -> z
+        find the shift value -> "z" - string[0]
+        shift every char in the string by the value
+        add key to the hashmap
         """
         
         groups = defaultdict(list)
@@ -14,10 +27,8 @@ class Solution:
             diff = ord("z") - ord(string[0])
             for char in string:
                 shifted = (ord(char) + diff) % 26
-                key += chr(shifted)
-            groups[key].append(string)
-            
-            
+                key += chr(ord("a") + shifted)
+            groups[key].append(string)  
         return groups.values()
         
         
