232d11548afb764313079244abbe166c7f3c603c
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-03-12 22:07:17 +0100

    Time: 78 ms (78.39%), Space: 26.3 MB (59.24%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,36 +6,19 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        """ 
-                      3
-                   /		\
-              5				 1
-           /    \		/		\
-        6		2		0		8
-            /		\
-            7	    4
-           /
-        11	
-        
-        LCA(11, 4) -> 2
-        LCA(11, 8) -> 3
-        LCA(4, 5) -> 5
-        LCA(6, 3) -> 3
-        Time O(N)
-        Space O(N)
-        """
-        return self.find_lca(root, p, q)
-    
-    def find_lca(self, root, node1, node2):
+    def lowestCommonAncestor(self, root: 'TreeNode', node1: 'TreeNode', node2: 'TreeNode') -> 'TreeNode':
         if not root:
             return None
-        if root.val == node1.val or root.val == node2.val:
-            return root
-
-        left_node = self.find_lca(root.left, node1, node2)
-        right_node = self.find_lca(root.right, node1, node2)
-
+        
+        if node1.val == root.val:
+            return node1
+        if node2.val == root.val:
+            return node2
+        
+        left_node = self.lowestCommonAncestor(root.left, node1, node2)
+        right_node = self.lowestCommonAncestor(root.right, node1, node2)
         if left_node and right_node:
             return root
+        
         return left_node or right_node
+        
