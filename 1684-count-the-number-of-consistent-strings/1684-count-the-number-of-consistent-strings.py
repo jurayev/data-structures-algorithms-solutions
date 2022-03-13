65c8d5bd3160a9f26dc42992b803cdd134d41830
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-03-13 16:48:26 +0100

    Time: 428 ms (22.99%), Space: 16.1 MB (66.00%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.py
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.py
@@ -1,14 +1,19 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        """
+        Count how many words you can make using given alphabet?
+        You can use as many letters as you want but only from the alpabet
+        
+        Input: alphabet = "ab", words = ["ad","bd","aaab","baa","badab"]
+        Output: 7
+        """
         alphabet = collections.Counter(allowed)
-        counter = 0
+        counter = len(words)
         for word in words:
             letters = Counter(word)
-            matched = 1
             for letter in letters:
                 if letter not in alphabet:
-                    matched = 0
+                    counter -= 1
                     break
-            counter += matched
                 
         return counter
