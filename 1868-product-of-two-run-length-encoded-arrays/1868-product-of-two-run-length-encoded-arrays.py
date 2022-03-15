5dc04866b334b2bc67111506a02f6e09c815dd86
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-03-15 18:57:25 +0100

    Time: 5505 ms (7.09%), Space: 71.4 MB (46.83%) - LeetHub

---
+++ 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findRLEArray(self, encoded1: List[List[int]], encoded2: List[List[int]]) -> List[List[int]]:
+        """
+        
+        Ideas:
+        Idea 1. Brute force - decode both arrays, do product of two arrays, encode it back
+            Time: O(nums1) + O(nums2) + O(output)
+            Space: O(nums1) + O(nums2) + O(output)
+        Idea 2. Two pointer iteration
+        Time: O(max(en1, en2))
+            Space: O(output)
+        if freq1[i] == freq2[i]:
+        Multiplying the val1[i] * val2[i]
+        Place the result in output[i] = [product, min_freq]
+        Move both p1 and p2
+        if freq1[i] < freq2[i]: move p1, decrement freq2[i] -= freq1[i]
+        if freq1[i] > freq2[i]: move p2, decrement freq1[i]
+
+        
+        """
+        encoded_product = []
+        p1, p2 = 0, 0
+        while p1 < len(encoded1) and p2 < len(encoded2):
+            val1, freq1 = encoded1[p1]
+            val2, freq2 = encoded2[p2]
+            product = val1 * val2
+            min_freq = min(freq1, freq2)
+            if encoded_product and encoded_product[-1][0] == product:
+                encoded_product[-1][1] += min_freq
+            else:    
+                encoded_product.append([product, min_freq])
+            if freq1 == freq2:
+                p1 += 1
+                p2 += 1
+            elif freq1 < freq2:
+                p1 += 1
+                encoded2[p2][1] -= min_freq
+            else:
+                p2 += 1
+                encoded1[p1][1] -= min_freq
+                
+        return encoded_product
+        
