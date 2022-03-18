47c7751e61d0de14b2d4e27ce47d65554e09ac48
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-03-18 21:44:36 +0100

    Time: 5122 ms (5.00%), Space: 15 MB (41.80%) - LeetHub

---
+++ 616-add-bold-tag-in-string/616-add-bold-tag-in-string.py
@@ -0,0 +1,56 @@
+class Solution:
+    def addBoldTag(self, s: str, words: List[str]) -> str:
+        """  012345678
+        s = "aaabbccam"
+              s
+                e
+          
+        bold_ranges = [[0,2],[1,3],[4,5]]
+                       [0,5]
+        
+        Time O(N^2)
+        Space O(N+M), M is the number of bold tags
+        """
+        
+        words_set = set(words)
+        n = len(s)
+        intervals = []
+        for start_idx in range(0, n):
+            for end_idx in range(start_idx+1, n+1):
+                word = s[start_idx:end_idx]
+                if word in words_set:
+                    intervals.append([start_idx, end_idx])
+        #print(intervals)           
+        merged = self.merge(intervals)
+        #print(merged)
+        output = list(s)
+        shifted_pos = 0
+        start_tag = "<b>"
+        end_tag = "</b>"
+        """
+        <>aaabbcc
+        
+        [[0,5]]
+        """
+        for start, end in merged:
+            output.insert(start+shifted_pos, start_tag)
+            shifted_pos += 1
+            output.insert(end+shifted_pos, end_tag)
+            shifted_pos += 1
+            
+        return "".join(output)
+    
+    def merge(self, intervals):
+        """
+        [[0,2],[1,3],[4,5]]
+                      ^
+        [[0,5]]
+        """
+        merged = []
+        for start, end in intervals:
+            if merged and merged[-1][1] >= start:
+                merged[-1][1] = max(merged[-1][1], end)
+            else:
+                merged.append([start, end])
+                
+        return merged
