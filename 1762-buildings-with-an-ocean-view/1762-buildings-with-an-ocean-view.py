86077473cdfb12bb0b36dc04a79b5ac63b386431
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-04-02 18:37:24 +0200

    Time: 995 ms (41.59%), Space: 32 MB (5.77%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -1,5 +1,9 @@
 class Solution:
     def findBuildings(self, buildings: List[int]) -> List[int]:
+        #return self.find_buildings_with_ocean_view_sol3(buildings)
+        return self.find_buildings_with_ocean_view_sol2(buildings)
+    
+    def find_buildings_with_ocean_view_sol3(self, buildings):
         tallest = 0
         n = len(buildings)
         buildings_with_view = []
@@ -9,3 +13,14 @@ class Solution:
                 buildings_with_view.append(index)
                 tallest = max(tallest, height)
         return buildings_with_view[::-1]
+    
+    def find_buildings_with_ocean_view_sol2(self, buildings):
+        increasing_stack = []
+
+        for index, height in enumerate(buildings):
+            while increasing_stack and buildings[increasing_stack[-1]] <= height:
+                increasing_stack.pop()
+
+            increasing_stack.append(index)
+
+        return increasing_stack
