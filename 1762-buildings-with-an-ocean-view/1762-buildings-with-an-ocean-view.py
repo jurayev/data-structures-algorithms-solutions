702fdc6848604f1b8323c3dff74269b736292f36
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-04-02 18:35:53 +0200

    Time: 768 ms (78.29%), Space: 31.7 MB (17.42%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -1,37 +1,11 @@
 class Solution:
-    def findBuildings(self, heights: List[int]) -> List[int]:
-        """
-        1. Brute Force try all positions
-            Time O(N^2)
-            Space O(N)
-        2. Use mono decreasing queue
-            Time O(N)
-            Space O(N)
-        3. Scan from right to left and keep a tallest building
-        
-        [4,2,3,3,1]
-         ^
-        3
-        [0, 3, 4]
-        """
-        tallest = float("-inf")
-        buildings = []
-        total = len(heights)
-        for index in reversed(range(total)):
-            building = heights[index]
-            if building > tallest:
-                tallest = building
-                buildings.append(index)
-                
-        return buildings[::-1]
-    
-    def mono_queue(heights):
-        mono_stack = []
-        for index in range(len(heights)):
-            height = heights[index]
-            while mono_stack and heights[mono_stack[-1]] <= height:
-                mono_stack.pop()
-                
-            mono_stack.append(index)
-            
-        return mono_stack
+    def findBuildings(self, buildings: List[int]) -> List[int]:
+        tallest = 0
+        n = len(buildings)
+        buildings_with_view = []
+        for index in range(n-1, -1, -1):
+            height = buildings[index]
+            if height > tallest:
+                buildings_with_view.append(index)
+                tallest = max(tallest, height)
+        return buildings_with_view[::-1]
