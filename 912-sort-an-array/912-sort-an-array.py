5a5249504e615d99dd7fe79a07b01d8046620a9f
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-04-16 12:05:18 +0200

    Time: 335 ms (93.15%), Space: 23.4 MB (15.68%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -11,8 +11,29 @@ class Solution:
         [] -> []
             [1][1] -> [1,1]
         """
+        return self.bucket_sort(nums)
         return self.merge_sort(nums)
+    
+    def bucket_sort(self, nums):
+        max_num = max(nums)
+        pos_buckets = [0] * (max_num + 1)
+        min_num = min(nums)
+        neg_buckets = [0] * (abs(min_num)+1) # [1, 2, 3, 5]
+        for num in nums:
+            if num >= 0:
+                pos_buckets[num] += 1
+            else:
+                neg_buckets[abs(num)] += 1
+        sorted_nums = []
+        for num in range(abs(min_num), -1, -1):
+            for _ in range(neg_buckets[num]):
+                sorted_nums.append(-num)
         
+        for num in range(0, max_num+1):
+            for _ in range(pos_buckets[num]):
+                sorted_nums.append(num)
+        return sorted_nums
+    
     def merge_sort(self, nums) -> list:
         # Time O(N log N)
         # Space O(N)
@@ -29,9 +50,10 @@ class Solution:
     def merge(self, left_part, right_part):
         array = []
         left_idx, right_idx = 0, 0
-        while left_idx < len(left_part) or right_idx < len(right_part):
-            left_el = left_part[left_idx] if left_idx < len(left_part) else float("inf")
-            right_el = right_part[right_idx] if right_idx < len(right_part) else float("inf")
+        left_size, right_size = len(left_part), len(right_part)
+        while left_idx < left_size or right_idx < right_size:
+            left_el = left_part[left_idx] if left_idx < left_size else float("inf")
+            right_el = right_part[right_idx] if right_idx < right_size else float("inf")
             if left_el < right_el:
                 array.append(left_el)
                 left_idx += 1
