60630aa0e891ed12fa9c0f5da33791fd81b37df9
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-04-18 10:45:08 +0200

    Time: 468 ms (94.38%), Space: 16.6 MB (39.68%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.py
+++ 743-network-delay-time/743-network-delay-time.py
@@ -1,66 +1,33 @@
-from heapq import heappush, heappop
-from collections import defaultdict
-class Edge:
-	def __init__(self, source, dest, cost):
-		self.source = source
-		self.dest = dest
-		self.cost = cost
-
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], nodes: int, start_node: int) -> int:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        
         graph = defaultdict(list)
-        for source, dest, cost in times:
-            graph[source].append(Edge(source, dest, cost))
-
-        # Implement Dijkstra, returns all costs
-        #costs = self.dijkstra(graph, start_node, nodes)
-        costs = self.bellman_ford_min_cost(times, start_node, nodes)
-        # vertex : cost mapping
-        # for every cost select the max cost and return
-        max_cost = max(costs)
-        return max_cost if max_cost != float("inf") else -1
-    
-    def bellman_ford_min_cost(self, times, start_node, nodes):
-        costs = [float("inf") for _ in range(nodes)]
-        costs[start_node-1] = 0
-        visited = set()
-        for source in range(1, nodes):
-            for source, dest, cost in times:
-                new_cost = costs[source-1] + cost
-                if new_cost < costs[dest-1]:
-                    costs[dest-1] = new_cost
-        return costs
-
-    def dijkstra(self, graph, start_node, nodes):
-        """
-        Time O(V + E LogV)
-        Space O(V + E)
         
-        start = 2
-        in_progess = [(0,2)* (1,1)* (1,3)* (2,4)*]
-                  1  2    3  4    5
-        costs = [1,  0,  1, 2,  inf]
-        visite = {2, 1, 3}
-        """
-        costs = [float("inf") for _ in range(nodes)]
-        costs[start_node-1] = 0
-        visited = set()
-        in_progress = []
-        heappush(in_progress, (0, start_node))
-        while in_progress:
-            cost, node = heappop(in_progress)
-            if costs[node-1] < cost:
-                # better cost for this node already found
+        for source, dest, time in times:
+            graph[source].append((time, dest))
+        return self.djikstra(graph, k, n)
+        
+        
+    def djikstra(self, graph, start_node, total_nodes):
+        max_time = 0
+        unvisited = [] # priority heap
+        heappush(unvisited, (0, start_node))
+        costs = [float("inf") for _ in range(total_nodes+1)]
+        costs[start_node] = 0
+        costs[0] = 0
+
+        while unvisited:
+            time, source = heappop(unvisited)
+            if costs[source] < time: # check if already better cost
                 continue
-            visited.add(node)
-            for edge in graph[node]:
-                if edge.dest in visited:
-                    # already visited
-                    continue
-                new_cost = cost + edge.cost
-                if new_cost < costs[edge.dest-1]:
-                    # relax the edge
-                    costs[edge.dest-1] = new_cost
-                    heappush(in_progress, (new_cost, edge.dest))
-        return costs
-        
+            max_time = max(max_time, time)
+            for dest_time, dest in graph[source]:
+                cost = time + dest_time
+                if costs[dest] > cost: # relax the edge
+                    costs[dest] = cost
+                    heappush(unvisited, (cost, dest))
+                    
+        for cost in costs:
+            if cost == float("inf"):
+                return -1
+        return max_time
