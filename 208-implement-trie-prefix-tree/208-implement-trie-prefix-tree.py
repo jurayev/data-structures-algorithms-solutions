8c05dcf8347ed43ecbe9fc6d2ccaf7bc2bbf2f3e
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-04-26 22:34:32 +0200

    Time: 122 ms (98.91%), Space: 27.5 MB (93.66%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -1,57 +1,43 @@
+class TrieNode:
+    def __init__(self,):
+        self.children = {}
+        self.is_word = False
+
 class Trie:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        
-        a:
-            p:
-                p:
-                    l:
-                        l:
-                    *:      e:
-                                *:
-        """
-        self.root = {}
-        self.anchor = "*"
-        
+        self.trie = {}
+        self.word_end = "*"
 
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
-        node = self.root
-        for char in word:
-            node[char] = node.get(char, {})
-            node = node[char]
-        node[self.anchor] = defaultdict()   
+        node = self.trie
         
+        for char in word:
+            if char not in node:
+                node[char] = {}
+            node = node[char]
+            
+        node[self.word_end] = True
 
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
-        node = self.root
+        node = self.trie
+        
         for char in word:
             if char not in node:
                 return False
             node = node[char]
-        return self.anchor in node
-
         
+        return self.word_end in node
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
-        node = self.root
+        node = self.trie
+        
         for char in prefix:
             if char not in node:
                 return False
             node = node[char]
+            
         return True
-
-        
 
 
 # Your Trie object will be instantiated and called as such:
