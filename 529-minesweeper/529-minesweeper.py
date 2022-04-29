e213d44490821f92d04e5c9dbcc4bbdcb6b3ed65
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-04-30 00:42:23 +0200

    Time: 195 ms (77.44%), Space: 16.8 MB (56.77%) - LeetHub

---
+++ 529-minesweeper/529-minesweeper.py
@@ -0,0 +1,55 @@
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        """
+        board = 
+        [["E","E","E","E","E"],
+         ["E","E","M","E","E"],
+         ["E","E","E","E","E"],
+         ["E","E","E","E","E"]], click = [3,0]
+         
+        Output: 
+        [["B","1","E","1","B"],
+         ["B","1","M","1","B"],
+         ["B","1","1","1","B"],
+         ["B","B","B","B","B"]]
+        
+        
+        """
+        if board[click[0]][click[1]] == "M":
+            board[click[0]][click[1]] = "X"
+        else:
+            self.dfs(board, click[0], click[1])
+        return board
+    
+    def check(self, board, x, y):
+        if not(0 <= x < len(board) and 0 <= y < len(board[0])):
+            return 0
+        if board[x][y] == "M":
+            return 1
+        return 0
+    
+    def dfs(self, board, x, y):
+        if not(0 <= x < len(board) and 0 <= y < len(board[0])):
+            return
+        if board[x][y] != "E":
+            return
+        mines = 0
+        dirs = [(1,0), (-1, 0), (0,1), (0,-1), (-1,-1),(-1,1),(1,-1),(1,1)]
+        for dirx, diry in dirs:
+            mines += self.check(board, x+dirx, y+diry)
+        if mines:
+            board[x][y] = str(mines)
+            return
+        board[x][y] = "B"
+        
+        self.dfs(board, x+1, y)
+        self.dfs(board, x-1, y)
+        self.dfs(board, x, y+1)
+        self.dfs(board, x, y-1)
+        
+        self.dfs(board, x-1, y-1)
+        self.dfs(board, x-1, y+1)
+        self.dfs(board, x+1, y-1)
+        self.dfs(board, x+1, y+1)
+        
+        return 0
