4daed4089b3d35093d049392bb470e010b25bf0e
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-04-30 18:49:51 +0200

    Time: 123 ms (65.23%), Space: 15.9 MB (37.85%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,7 +1,7 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        #return self.dp_solution(height)
-        return self.two_pointers_solution(height)
+        return self.dp_solution(height)
+        #return self.two_pointers_solution(height)
         
         
     def dp_solution(self, heights):
