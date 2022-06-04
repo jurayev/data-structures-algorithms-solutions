100c5e557b9ddd6cfb11a4a092decc4eed89c929
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-06-04 16:30:55 +0200

    Time: 208 ms (8.66%), Space: 14.1 MB (21.59%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -0,0 +1,45 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        """
+        [2,5,2,1,2]
+        
+        [1,2,2,2,5] t=5
+             ^
+        [122]
+        [5]
+        
+        [10,1,2,7,6,1,5]
+        [1,1,2,5,6,7,10]
+        
+        [1]
+          [11]
+            [112]
+          [12]
+        [1]
+          [1,2]
+        """
+        candidates.sort()
+        def generate(idx, target, curr):
+            if target <= 0:
+                if target == 0:
+                    combs.append(list(curr))
+                return
+            
+            idj = idx
+            while idj < len(candidates):
+                curr.append(candidates[idj])
+                generate(idj+1, target-candidates[idj], curr)
+                curr.pop()
+                
+                while idj+1 < len(candidates) and candidates[idj] == candidates[idj+1]:
+                    idj += 1
+                idj += 1
+                
+                
+                
+        
+        combs = []
+        curr = []
+        generate(0, target, curr)
+        
+        return combs
