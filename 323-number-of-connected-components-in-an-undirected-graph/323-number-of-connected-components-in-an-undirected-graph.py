22af548da0d257c8adb200ec421bc128578b1d65
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-06-18 20:37:36 +0200

    Time: 222 ms (12.20%), Space: 15.6 MB (50.57%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,5 +1,45 @@
+class UnionFind:
+    def __init__(self, size):
+        self.size = size
+        self.sizes = [1 for _ in range(self.size)]# node  0 1 2 3 4 
+        self.sets = [i for i in range(self.size)] # root [0,0,0,3,3]
+    
+    def find(self, node):
+        if node == self.sets[node]:
+            return node
+        self.sets[node] = self.find(self.sets[node])
+        return self.sets[node]
+    
+    def union(self, node1, node2):
+        root1 = self.find(node1)
+        root2 = self.find(node2)
+        
+        if root1 != root2:
+            if self.sizes[root1] > self.sizes[root2]:
+                self.sets[root2] = self.sets[root1]
+                self.sizes[root1] += self.sizes[root2]
+            else:
+                self.sets[root1] = self.sets[root2]
+                self.sizes[root2] += self.sizes[root1]
+            return 1
+        return 0
+                
+                
+        
+        
+
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        union_find = UnionFind(n)
+        graph = defaultdict(list)
+        components = n
+        for start, end in edges:
+            components -= union_find.union(start, end)
+       
+        return components
+        
+    
+    def countComponents_dfs_bfs(self, n: int, edges: List[List[int]]) -> int:
         graph = defaultdict(list)
         
         for start, end in edges:
