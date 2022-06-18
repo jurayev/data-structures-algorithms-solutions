b898c3352762ff4caaca69b3da95b2c38410ff3f
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-06-18 20:15:11 +0200

    Time: 156 ms (43.60%), Space: 15.7 MB (44.65%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -9,11 +9,24 @@ class Solution:
         visited = set()
         for node in range(n):
             if node not in visited:
-                self.dfs(node, graph, visited)
+                self.dfs_iterative(node, graph, visited)
                 components += 1
         return components
-
-    def dfs(self, source, graph, visited):
+    
+    def dfs_iterative(self, source, graph, visited):
+        
+        stack = [source]
+        
+        while stack:
+            source = stack.pop()
+            visited.add(source)
+            for dest in graph[source]:
+                if dest not in visited:
+                    stack.append(dest)
+        
+        
+        
+    def dfs_recursive(self, source, graph, visited):
         
         visited.add(source)
         
