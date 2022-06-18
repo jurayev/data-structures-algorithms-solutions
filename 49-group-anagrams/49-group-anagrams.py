fdf4d1dbb5e87e314f6f232409590cda7d3241ca
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2022-06-18 15:03:21 +0200

    Time: 296 ms (5.01%), Space: 18.3 MB (44.98%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -4,6 +4,23 @@ class Solution:
         groups = defaultdict(list)
         
         for string in strs:
+            alphabet = [0] * 26
+            
+            for char in string:
+                idx = ord(char) - ord("a")
+                alphabet[idx] += 1
+            hash_key = "#".join(map(str, alphabet))
+            groups[hash_key].append(string)
+            
+        return groups.values()
+        
+    
+    
+    def groupAnagrams1(self, strs: List[str]) -> List[List[str]]:
+        
+        groups = defaultdict(list)
+        
+        for string in strs:
             normalized = "".join(sorted(list(string)))
             groups[normalized].append(string)
             
